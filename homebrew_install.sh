fb6a94f2e68f62daf55a65f17b7a41a29f3e2351
ryohei-ishibashi <ryohei808080@gmail.com>
2022-02-23 19:02:01 +0900

    fix

--- homebrew_install.sh
+++ homebrew_install.sh
@@ -15,40 +15,133 @@ brew upgrade --all
 
 # インストールするモジュール群
 formulas=(
-  git
-  wget
-  curl
-  tree
-  openssl
-  z
-  colordiff
-  "--without-etcdir zsh"
-  zsh-completions
-  "--wish-cocoa --srgb emacs"
-  cask
-  ansible
-  peco
-  hub
-  tig
-  node
-  python3
-  lua
-  "vim --with-lua"
-  mysql
-  sqlite
-  "php55 --homebrew-apxs"
-  httpd22
-  ricty
-  sqlite
-  composer
-  markdown
-  ctags
-  ssh-copy-id
-  phantomjs
-  mecab
-  diff-so-fancy
-  thefuck
-  zsh
+    ansible
+    emacs
+    heroku-node
+    libtiff
+    m4
+    peco
+    tree
+    apr
+    fontconfig
+    hub
+    libtool
+    markdown
+    php
+    tree-sitter
+    apr-util
+    fontforge
+    icu4c
+    libuninameslist
+    mecab
+    php@7.4
+    unbound
+    argon2
+    freetds
+    jansson
+    libunistring
+    mpdecimal
+    pixman
+    unibilium
+    aspell
+    freetype
+    jpeg
+    libuv
+    msgpack
+    pkg-config
+    unixodbc
+    autoconf
+    fribidi
+    krb5
+    libvterm
+    mysql
+    protobuf
+    webp
+    bdw-gc
+    gd
+    lha
+    libx11
+    ncurses
+    pstree
+    wget
+    brotli
+    gdbm
+    libevent
+    libxau
+    neovim
+    python@3.10
+    xorgproto
+    c-ares
+    gettext
+    libffi
+    libxcb
+    nettle
+    python@3.9
+    xz
+    ca-certificates
+    giflib
+    libidn2
+    libxdmcp
+    node
+    rbenv
+    z
+    cairo
+    git
+    libnghttp2
+    libxext
+    nodebrew
+    readline
+    zsh
+    cask
+    glib
+    libpng
+    libxrender
+    oniguruma
+    rtmpdump
+    zsh-completions
+    colordiff
+    gmp
+    libpq
+    libyaml
+    openldap
+    ruby-build
+    zstd
+    composer
+    gnutls
+    libpthread-stubs
+    libzip
+    openssl@1.1
+    six
+    coreutils
+    gobject-introspection
+    libsodium
+    lua
+    openssl@3
+    sqlite
+    ctags
+    graphite2
+    libspiro
+    luajit-openresty
+    p11-kit
+    ssh-copy-id
+    curl
+    guile
+    libssh2
+    luv
+    pango
+    thefuck
+    deno
+    harfbuzz
+    libtasn1
+    lz4
+    pcre
+    tidy-html5
+    diff-so-fancy
+    heroku
+    libtermkey
+    lzo
+    pcre2
+    tig
 )
 
 "brew tap..."
@@ -60,20 +153,20 @@ brew tap sanemat/font
 
 echo "start brew install apps..."
 for formula in "${formulas[@]}"; do
-  brew install $formula || brew upgrade $formula
+brew install $formula || brew upgrade $formula
 done
 
 cask=(
-  google-chrome
-  slack
-  iterm2
-  "visual-studio-code"
+    chromedriver
+    phantomjs
+    iterm2
+    "visual-studio-code"
 )
 
 echo "start brew cask install apps..."
 
 for cask in "${casks[@]}"; do
-  brew cask install $cask
+brew cask install $cask
 done
 
 brew cleanup
